99d4890bb9c321479690b70a898ccbae157b2f3a
Xeroday <eric@xeroday.org>
2014-01-07 19:47:48 -0500

    Start conversion to C#, finish GetArtist()

---
+++ EZBlocker/EZBlocker/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace EZBlocker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.BlockButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.AutoAddCheck = new System.Windows.Forms.CheckBox();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // BlockButton
+            // 
+            this.BlockButton.Location = new System.Drawing.Point(12, 36);
+            this.BlockButton.Name = "BlockButton";
+            this.BlockButton.Size = new System.Drawing.Size(140, 36);
+            this.BlockButton.TabIndex = 0;
+            this.BlockButton.Text = "Add Current to Blocklist";
+            this.BlockButton.UseVisualStyleBackColor = true;
+            this.BlockButton.Click += new System.EventHandler(this.BlockButton_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(158, 36);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(138, 36);
+            this.OpenButton.TabIndex = 1;
+            this.OpenButton.Text = "Open Blocklist";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            // 
+            // AutoAddCheck
+            // 
+            this.AutoAddCheck.AutoSize = true;
+            this.AutoAddCheck.Checked = true;
+            this.AutoAddCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AutoAddCheck.Location = new System.Drawing.Point(12, 87);
+            this.AutoAddCheck.Name = "AutoAddCheck";
+            this.AutoAddCheck.Size = new System.Drawing.Size(211, 17);
+            this.AutoAddCheck.TabIndex = 2;
+            this.AutoAddCheck.Text = "AutoAdd Ads to Blocklist (Experimental)";
+            this.AutoAddCheck.UseVisualStyleBackColor = true;
+            this.AutoAddCheck.CheckedChanged += new System.EventHandler(this.AutoAddCheck_CheckedChanged);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Enabled = true;
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 373);
+            this.Controls.Add(this.AutoAddCheck);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.BlockButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.RightToLeftLayout = true;
+            this.Text = "EZBlocker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BlockButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.CheckBox AutoAddCheck;
+        private System.Windows.Forms.Timer MainTimer;
+    }
+}
+
---
+++ EZBlocker/EZBlocker/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EZBlocker
+{
+    public partial class Main : Form
+    {
+        private String title = String.Empty; // Title of the Spotify window
+        private Boolean autoAdd = true;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateTitle();
+        }
+
+        /**
+         * Updates the title of the Spotify window.
+         * 
+         * Returns true if title updated successfully, false if otherwise
+         **/
+        private Boolean UpdateTitle()
+        {
+            Process[] p = Process.GetProcesses();
+            for (var i = 0; i < p.Length; i++)
+            {
+                if (p[i].ProcessName.Equals("spotify"))
+                {
+                    title = p[i].MainWindowTitle;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * Determines whether or not Spotify is currently playing
+         **/
+        private Boolean IsPlaying()
+        {
+            return title.Contains(" - ");
+        }
+
+        private String GetArtist()
+        {
+            if (!IsPlaying()) return String.Empty;
+            Debug.WriteLine("2");
+            return title.Substring(10).Split('\u2013')[0].TrimEnd(); // Split at endash
+        }
+
+        private void BlockButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("1");
+            Debug.WriteLine(GetArtist());
+        }
+
+        private void AutoAddCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            autoAdd = AutoAddCheck.Checked;
+        }
+
+    }
+}
