71039b4545b4dce6697b62f4b7e7a62b9d1db97c
Maschmi <Maschmi@users.noreply.github.com>
2016-06-16 21:31:09 +0200

    Generate codecoverage and upload to codecov

--- coverage.bat
+++ coverage.bat
@@ -1,6 +1,6 @@
 mkdir TestResults
 
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -filter:"+[*]* -[Tests*]*" "-target:C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" "-targetargs:/testcontainer:.\src\Tests\Tests.Model\bin\Debug\Tests.Model.dll /testcontainer:.\src\Tests\Tests.SpotifyMuter.Logic\bin\Debug\Tests.SpotifyMuter.Logic.dll" "-output:.\TestResults\coverage.xml"
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -returntargetcode -register:user -filter:"+[*]* -[Tests*]*" "-target:C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" "-targetargs:/testcontainer:.\src\Tests\Tests.Model\bin\Debug\Tests.Model.dll /testcontainer:.\src\Tests\Tests.SpotifyMuter.Logic\bin\Debug\Tests.SpotifyMuter.Logic.dll" "-output:.\TestResults\coverage.xml"
 
 .\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe "-reports:.\TestResults\coverage.xml" "-targetdir:.\TestResults\Coverage"
 
