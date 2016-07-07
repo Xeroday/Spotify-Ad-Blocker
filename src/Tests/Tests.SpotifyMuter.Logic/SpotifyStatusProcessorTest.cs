6be356df913b7b0d9f735c55f49ece09be916689
Maschmi <Maschmi@users.noreply.github.com>
2016-07-07 18:16:42 +0200

    Debug/Release builds and publish release drafts for tags (#6)
    Debug/Release builds and publish release drafts for tags

--- src/Tests/Tests.SpotifyMuter.Logic/SpotifyStatusProcessorTest.cs
+++ src/Tests/Tests.SpotifyMuter.Logic/SpotifyStatusProcessorTest.cs
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see<http://www.gnu.org/licenses/>.*/
 
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
 using Moq;
