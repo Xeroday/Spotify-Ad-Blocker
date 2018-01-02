4c122f711a247412540ceda66282702fbe2dc01f
andriyDev <andriydzikh@gmail.com>
2018-01-02 10:08:45 -0500

    User will not be told that EZBlocker will no longer block ads if turned off, if exitOnClose is disabled (this is for turning off your computer.

--- EZBlocker/EZBlocker/Form1.cs
+++ EZBlocker/EZBlocker/Form1.cs
@@ -728,18 +728,21 @@ namespace EZBlocker
 
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            if (!Properties.Settings.Default.UserEducated)
+            if (exitOnClose)
             {
-                var result = MessageBox.Show("Spotify ads will not be muted if EZBlocker is not running.\r\n\r\nAre you sure you want to exit?", "EZBlocker",
-                                 MessageBoxButtons.YesNo,
-                                 MessageBoxIcon.Warning);
-
-                e.Cancel = (result == DialogResult.No);
-
-                if (result == DialogResult.Yes)
+                if (!Properties.Settings.Default.UserEducated)
                 {
-                    Properties.Settings.Default.UserEducated = true;
-                    Properties.Settings.Default.Save();
+                    var result = MessageBox.Show("Spotify ads will not be muted if EZBlocker is not running.\r\n\r\nAre you sure you want to exit?", "EZBlocker",
+                                     MessageBoxButtons.YesNo,
+                                     MessageBoxIcon.Warning);
+
+                    e.Cancel = (result == DialogResult.No);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        Properties.Settings.Default.UserEducated = true;
+                        Properties.Settings.Default.Save();
+                    }
                 }
             }
         }
