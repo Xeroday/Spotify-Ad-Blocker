f113dd5cb6a738a266cff51b9614251420695dce
Adenw His <sthy14@icloud.com>
2018-07-23 13:01:55 +0100

    Update SpotifyHook.cs
    This change also seems resolved the pausing problem. Seems that sometimes pausing would cause WindowName to be empty.

--- EZBlocker/EZBlocker/SpotifyHook.cs
+++ EZBlocker/EZBlocker/SpotifyHook.cs
@@ -38,7 +38,7 @@ namespace EZBlocker
 
         public bool IsAdPlaying()
         {
-            return IsPlaying() && !WindowName.Contains(" - ") && !WindowName.Equals("Drag");
+            return IsPlaying() && !WindowName.Contains(" - ") && !WindowName.Equals("Drag") && !string.IsNullOrWhiteSpace(WindowName);
         }
 
         public bool IsRunning()
